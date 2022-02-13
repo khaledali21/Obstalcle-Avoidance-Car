98cd0544fefa0be81fa0896ae86a1d3e1d1dc762
Khaled Ali <khaled21.ali33@gmail.com>
2022-02-13 12:40:26 +0200

    First Commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/*
+ * Obstacle-Avoidance-Car.c
+ *
+ * Created: 2/13/2022 12:37:15 PM
+ * Author : Khaled Ali
+ */ 
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
